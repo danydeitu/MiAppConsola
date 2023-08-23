c7f5014fe09ada8a29fe31a4e6f6e1e35daefef7
Daniel <danydeitu18@gmail.com>
2023-08-23 08:41:58 -0300

    add console .net

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");*/
+using System;
+
+namespace MiAppConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola mundo, ejecutando desde mi aplicación de consola en C#!");
+        }
+    }
+}
+
